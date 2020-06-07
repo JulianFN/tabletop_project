f2b695bd88e9cb4dd884ba2d7c02307a88d7bd5e
Trevor <Trevorauten@gmail.com>
2020-06-06 21:52:34 -0500

    Updated imports for game. Created index.js

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './global.css';
+import Game from './game.js';
+
+ReactDOM.render(
+    <Game />,
+    document.getElementById('root')
+  );
